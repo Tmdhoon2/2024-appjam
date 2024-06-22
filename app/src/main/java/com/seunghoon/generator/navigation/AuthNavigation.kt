f22db3c2d527b9dc20f98788ab630cac895e6ee8
Tmdhoon2 <hks026naver@gmail.com>
2024-06-22 18:19:44 +0900

    feat :: webview 설정

--- app/src/main/java/com/seunghoon/generator/navigation/AuthNavigation.kt
+++ app/src/main/java/com/seunghoon/generator/navigation/AuthNavigation.kt
@@ -1,8 +1,21 @@
 package com.seunghoon.generator.navigation
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.ViewGroup
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import org.json.JSONObject
 
 internal fun NavGraphBuilder.auth() {
     navigation(
@@ -12,5 +25,36 @@ internal fun NavGraphBuilder.auth() {
         composable(NavigationRoute.Auth.SPLASH) {
 
         }
+    }
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+internal fun WebViewTest() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        AndroidView(
+            factory = {
+                WebView(it).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webViewClient = WebViewClient()
+
+                    settings.javaScriptEnabled = true
+
+                    addJavascriptInterface(WebViewInterface(context), "android")
+
+                    loadUrl("https://appjam.vercel.app")
+                }
+            },
+        )
+    }
+}
+
+class WebViewInterface(private val context: Context) {
+    @JavascriptInterface
+    fun getToken(json: String) {
+        Toast.makeText(context, json, Toast.LENGTH_SHORT).show()
     }
 }
